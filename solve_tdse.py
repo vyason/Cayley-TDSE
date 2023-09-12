10113e2912b64f536a10d006746dfede65abc7cd
Ankit Kumar <kumar.ankit.vyas@gmail.com>
2023-09-12 10:25:32 +0530

    Update solve_tdse.py

--- solve_tdse.py
+++ solve_tdse.py
@@ -23,6 +23,7 @@
 import numpy as np
 from math import pi
 from scipy.integrate import simps
+import matplotlib.pyplot
 
 from calc_diff import *
 
@@ -35,7 +36,7 @@ disc_type = 'tridiag'
 hb = 1                                  #Planck's constant, hbar
 m =	1                                   #mass of particle, m
 
-xmin,xmax = -25,+25                     #x-limits of simulation box
+xmin,xmax = 0,+10                     #x-limits of simulation box
 dx = 0.01                               #grid size, dx 
 x = np.arange(xmin,xmax+dx,dx)          #defining the position grid
 J = len(x)                              #dimension of position grid
@@ -44,18 +45,17 @@ J = len(x)                              #dimension of position grid
 #-----------------------------------------------------------------------
 w = 0.1
 V = 1/2*m*w**2*x**2
+V = np.zeros(J,int)
 
 # defining the initial wave packet (for example, Gaussian)
 #-----------------------------------------------------------------------
-x0 = -10                                #initial position
-p0 = 0                                  #initial momentum            
-sig = 2                                 #initial position spread
+psi = np.sqrt(2/L)*np.sin(pi*x/L)
 
-psi = np.exp( -((x-x0)/(2*sig))**2 + 1j*p0*(x-x0) )/np.sqrt( sig*np.sqrt(2*pi) )
+
 
 # time limits for the simulation
 #-----------------------------------------------------------------------
-tmax = 2*pi/w                           #simulation time limit
+tmax = 100                           #simulation time limit
 dt = 0.01                               #time step, dt
 plot_steps = 10                         #time steps b/w two successive print statements
 
@@ -82,8 +82,8 @@ while t < tmax:
     dpsi = diff_5pt(psi,dx,J)                               #d(psi)/dx
     ep = (-1j*hb*simps(np.conj(psi)*dpsi,dx=dx)).real       #<p>
 
-    print(f'{w*t:f}\t{ex:e}\t{ep:e}')
-
     for j in range(plot_steps):                             #evolve plot_steps times
         psi = lhs_lu.solve(zeta(J,psi,dx,dt,V,hb,hb2m))   
     t = t + plot_steps*dt
+
+    plt.
